25af048ccc68ea16e8b9b0e086b27fe632a3daa5
Yauhen Siniauski <yauhen.s@ecarstrade.com>
2025-10-05 16:44:46 +0200

    CORS fix

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/main', (req, res) => {
